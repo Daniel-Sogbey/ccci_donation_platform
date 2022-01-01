c7ef590955b005c95305a18803ed1a3f5eea5da6
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-01-01 13:32:22 +0000

    updated port

--- app.js
+++ app.js
@@ -105,6 +105,6 @@ app.get("/error", (req, res) => {
 	res.render("error.pug");
 });
 
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server ON, port ${PORT}`));
